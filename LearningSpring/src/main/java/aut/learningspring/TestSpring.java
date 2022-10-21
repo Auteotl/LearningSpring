d45946f16fbb75648f344b107364b6b4231d31e4
Auteotl <93289809+Auteotl@users.noreply.github.com>
2022-10-21 12:03:30 +0300

    1st lesson

---
+++ LearningSpring/src/main/java/aut/learningspring/TestSpring.java
@@ -0,0 +1,16 @@
+package aut.learningspring;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class TestSpring {
+    public static void main(String[] args) {
+        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext(
+               "applicationContext.xml"
+        );
+        TestBean testbean = context.getBean("testBean", TestBean.class);
+
+        System.out.println(testbean.getName());
+
+        context.close();
+    }
+}
